9e9d9316f3ef9ce41376cb536641f1d08afce8a1
frederet <ethan.frederick@baxter-academy.org>
2017-02-02 14:25:20 -0500

    Web App with Connected Pages

--- web app/flask/run.py
+++ web app/flask/run.py
@@ -3,7 +3,19 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def start():
+def login():
 	return render_template('login.html')
 
-app.run()
+@app.route('/teacher')
+def teacher():
+	return render_template('teacher.html')
+
+@app.route('/studentpage1')
+def studentpage1():
+	return render_template('studentpage1.html')
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
